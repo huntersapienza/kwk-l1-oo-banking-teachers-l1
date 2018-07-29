ad5ce40607b0635bd413b3a71d5ab2560f2c7dee
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-29 17:26:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,10 @@ class Transfer
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
-    
   end
   
+  def reverse_transfer
+    sender.balance += amount
+    receiver.balance -= amount
+    @status = "reversed"
 end
