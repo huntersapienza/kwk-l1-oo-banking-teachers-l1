bc946af623a206e78422deff6c9fe318b3231cd3
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-29 17:25:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
       @status = "rejected"
     end
   end
