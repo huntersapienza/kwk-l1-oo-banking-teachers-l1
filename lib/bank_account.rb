a2a0a56357c72c4929caad0c12969a7801d8da54
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-29 16:58:28 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,5 @@
 class BankAccount
-  attr_accessor :name, :balance, status
+  attr_accessor :name, :balance, :status
   def initialize(name, balance = 1000, status = open)
     @name = name
     @balance = balance
